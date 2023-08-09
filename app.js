dca3c8e6dba42c88e3bc6ddff8c862fd9a51357d
sundarasekar <sundarasekar01@gmail.com>
2023-08-09 21:50:01 +0530

    node-mailer

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const express = require("express")
-const app= express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine','ejs')
+
+
+
+
 
 var nodemailer = require('nodemailer');
 
@@ -7,32 +15,38 @@ var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'sundarasekar01@gmail.com',
-    pass: 'yourpassword'
+    pass: 'byopnoxulklfskuz'
   }
 });
 
-var mailOptions = {
-  from: 'sundarasekar01@gmail.com',
-  to: 'sundarasekar2308@gmail.com',
-  subject: 'Node-mailer',
-  text: 'Its working perfectly'
-};
+app.get('/', (req, res) => {
+    const message = req.query.message;
+  res.render("../index",{message})
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
 });
 
+app.post('/send-email', (req, res) => {
+  const { to, subject, message } = req.body;
 
+  var mailOptions = {
+    from: 'sundarasekar01@gmail.com',
+    to: to,
+    subject: subject,
+    text: message
+  };
 
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+        res.redirect('/?message=Error sending email');
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.redirect('/?message=Email sent successfully'); 
+      }
+  });
+});
 
+app.listen(3000, () => {
+  console.log("connected to 3000"); 
+});
 
-
-
-
-app.listen(2000,()=>{
-    console.log("connected to 2000")
-})
